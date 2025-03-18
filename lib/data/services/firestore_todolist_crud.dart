9f2994ddeba33e3b0988fa24d58ead1eac9c32e9
e2McNuts <120487956+e2McNuts@users.noreply.github.com>
2025-03-18 18:50:05 +0100

    .

--- lib/data/services/firestore_todolist_crud.dart
+++ lib/data/services/firestore_todolist_crud.dart
@@ -2,20 +2,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreTodolistCRUD {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final String todolist = 'todoLists';
 
   Stream<QuerySnapshot> getTodolistsStream() {
-    return _firestore.collection('todoLists').snapshots();
+    return _firestore.collection(todolist).snapshots();
   }
 
   Future<void> deleteTodolist(String docID) {
-    return _firestore.collection('todoLists').doc(docID).delete();
+    return _firestore.collection(todolist).doc(docID).delete();
   }
 
   Future<void> updateTodolist(String docID, Map<String, dynamic> data) {
-    return _firestore.collection('todoLists').doc(docID).update(data);
+    return _firestore.collection(todolist).doc(docID).update(data);
   }
 
   Future<void> addTodolist(Map<String, dynamic> data) {
-    return _firestore.collection('todoLists').add(data);
+    return _firestore.collection(todolist).add(data);
   }
 }
